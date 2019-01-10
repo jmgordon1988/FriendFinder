7cd63d8ba131e555cf050106dab8307733c89df5
John Gordon <jmgordon1988@gmail.com>
2019-01-09 17:11:21 -0800

    working version

---
+++ app/data/friends.js
@@ -0,0 +1,174 @@
+var friendsArray = [
+    {
+        name: 'Loki',
+        photo: 'https://icdn3.digitaltrends.com/image/thor-dark-world-ss28.jpg?ver=1',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ],
+        desc: 'Loki is a Norse god of mischief, as well as fatherhood.'
+    },
+    {
+        name: 'Cthulhu',
+        photo: 'https://www.telegraph.co.uk/content/dam/films/2016/10/28/cthulhu_trans_NvBQzQNjv4BqeWq0Odl7YRxHNYM74_QBWlbFJiGQSGUwQFXFdwSXZiw.jpg?imwidth=450',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ],
+        desc: 'Cthulhu is a Great Old One of great power that lies in a death-like slumber beneath the Pacific Ocean in his sunken city of Rlyeh. He remains a dominant presence in the eldrich dealings on our world.'
+    },
+    {
+        name: 'Pax',
+        photo: 'https://i.redd.it/51sp5ex3oa721.jpg',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ],
+        desc: 'Pax is the thief of fate, stealing free will from his subjects so they serve his bidding.'
+    },
+    {
+        name: 'Moander',
+        photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSFretYmuwLvQA84m-rImQp2FoPWEHsR5iS0O03fKUVHdgCcN',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ],
+        desc: 'Moander was an ancient deity of rot, corruption, and decay. The Darkbringer was generally a cruel, controlling tyrant, delighting in the torment and destruction of lesser beings and seeking to corrupt any who refused to bow down to it.'
+    },
+    {
+        name: 'Baphomet',
+        photo: 'https://adeptinitiates.com/wp-content/uploads/2018/05/31582292_10211823932540719_1993854381514031104_n-1150x953.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ],
+        desc: 'Baphomet, Demon Lord of the Minotaurs, rules over the Abyssal realm known as the Ivory Labyrinth. He was created to lead his mistress and her hordes in the mortal world.'
+    },
+    {
+        name: 'Bahamut',
+        photo: 'https://dnd-wiki.org/w/images/thumb/d/df/Bahamut.jpg/600px-Bahamut.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ],
+        desc: 'Bahamut is the dragon god of justice and a subservient deity to Torm, god of law. Before entering the Faerûnian pantheon, he was a member of the Draconic pantheon, as a deity of good dragons, metallic dragons, wisdom, and enlightened justice (justice tempered with mercy and punishment with forgiveness) known by the name of Xymor.'
+    },
+    {
+        name: 'Tyr',
+        photo: 'https://i.ytimg.com/vi/GnfDSbmWLws/hqdefault.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ],
+        desc: 'Tyr, also known as The Maimed God, was a lawful good greater deity of law and justice and part of The Triad. His dogma was primarily concerned with the punishment of wrong-doers, and the general furthering of law and good in the world. '
+    },
+    {
+        name: 'Azathoth',
+        photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpghttps://vignette.wikia.nocookie.net/the-many-adventures-of-minecraft-rogers/images/6/6d/Ia_Ia_Azathoth.jpg/revision/latest?cb=20171116100951',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ],
+        desc: 'Azathoth, also known as The Blind Idiot God, The Daemon Sultan and The Nuclear Chaos, is the overarching antagonist in the Cthulhu Mythos by the late horror fantasy writer H.P. Lovecraft. He is a boundless deity of colossal proportion. He is the great and all-powerful ruler of the Outer Gods.'
+    },
+    {
+        name: 'Shuma-Gorath',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/95/Shuma_Gorath.jpg/250px-Shuma_Gorath.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ],
+        desc: 'Shuma-Gorath is an evil deity from the Marvel universe, and is one of the Many-Angled Ones. He is an ancient alien deity that demands blood sacrifice, most commonly serving as an enemy to Doctor Strange, Conan the Barbarian, and the Guardians of the Galaxy.'
+    },
+    {
+        name: 'Elmo',
+        photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ],
+        desc: 'None can stop the horrors of the true malevolent ruler of the multiverse, The Crimson Laugh, The Beast With No Mercy, Elmo.'
+    }
+];
+
+module.exports = friendsArray;
